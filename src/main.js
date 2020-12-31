189091171de0a152a399d8f8a7d11a4da2064004
vasurajan <63305945+vasurajan@users.noreply.github.com>
2020-12-31 19:14:23 +0530

    url changed from http to https

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import router from './router'
 import axios from 'axios'
 
 Vue.config.productionTip = false
-axios.defaults.baseURL = 'http://www.omdbapi.com/?apikey=b76b385c&page=1&type=movie&Content-Type=application/json'
+axios.defaults.baseURL = 'https://www.omdbapi.com/?apikey=b76b385c&page=1&type=movie&Content-Type=application/json'
 new Vue({
   render: h => h(App),
   store,
